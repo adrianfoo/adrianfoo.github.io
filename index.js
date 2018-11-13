db4ed40154ebdc4e3da7f4baad554d7485c33ed3
Glitch (ict2x01-team10) <none>
2018-11-13 14:30:13 +0000

    added scores and updated collision

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+updateScore(totalScore);
+
 var msg = document.querySelector("#msg1");
 msg.text= 'align:center; width:3; wrapCount:100; color:red; value:Stay on path.;'
 
 var cam = document.querySelector('#cam');
 
 var pos = cam.getAttribute('position');
+var x = pos.x;
+var y = pos.y;
 cam.addEventListener('componentchanged', function (evt) {
   //console.log('CAMERA x/y:', pos.x, pos.y);
   if(pos.x < -2){
@@ -14,6 +18,8 @@ cam.addEventListener('componentchanged', function (evt) {
       color:'red',
       value:'Stay on path'
     });
+    totalScore -= 1;
+    updateScore(totalScore);
   }
   else if(pos.x > 2){
     msg.setAttribute('text', {
@@ -23,7 +29,8 @@ cam.addEventListener('componentchanged', function (evt) {
       color:'red',
       value:'Stay on path'
     });
-    
+    totalScore -= 1;
+    updateScore(totalScore);
   }
   else{
       msg.setAttribute('text', {
@@ -34,6 +41,8 @@ cam.addEventListener('componentchanged', function (evt) {
       value:''
       });
   }
+  x = pos.x;
+  y = pos.y;
     return; 
 });
 
