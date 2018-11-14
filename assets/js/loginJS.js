bca2ccb93a21642ad739ee9634e8575b229c3862
Lim Jie Hong <44803854+lostboi1995@users.noreply.github.com>
2018-11-14 14:50:06 +0800

    Added Login Page

---
+++ assets/js/loginJS.js
@@ -0,0 +1,35 @@
+var attempt = 5; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("form-username").value;
+    var password = document.getElementById("form-password").value;
+    if (username == "" || password == "" ){
+        alert ("Please enter your username and password!");
+        return false;
+    }
+    else if ( username == "admin" && password == "admin123"){
+        alert ("Admin Login successfully!");
+        window.location = "adminmenu.html"; // Redirecting to other page.
+        return false;
+    }
+    else if (username == "player" && password == "player123"){
+        alert ("Player Login successfully!");
+        window.location = "scene1.html"; // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("Invalid username/password!\nYou have "+attempt+" attempt left!");
+        // Disabling fields after 5 attempts.
+        if( attempt == 0){
+            document.getElementById("form-username").disabled = true;
+            document.getElementById("form-password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+
+
+
